0ec297ba5dd9444635362d1b390dd572198f1114
ashok2424 <97282425+ashok2424@users.noreply.github.com>
2022-01-21 19:11:59 +0530

    Time: 4 ms (97.45%), Space: 9.5 MB (86.84%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         long mask = 1,ans = 0, p = 1;
-        for(int i=0;i<33;i++){
+        for(int i=0;i<32;i++){
             int c=0;
             for(auto &e:nums){
                 if((e&mask)!=0){ 
