9250451c8062d842f6eb7e33f010857a4f113666
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-22 12:39:15 +0530

    Time: 38 ms (35.29%), Space: 11.1 MB (18.50%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -19,9 +19,31 @@ public:
         return ans;
     }
 };/*
-
- 9           25          9
-'a'  d f c  'a'  b f v  'a'
- 0   1 2 3   4   5 6 7   8 
  
- */
+contribution  9           25          9
+             'a'  d f c  'e'  b z v  'i'
+indices       0   1 2 3   4   5 6 7   8 
+
+just take an example tets case and ull catch the formula
+ 
+=>as there are no prev to a, only subtrings starting with itself can make all the contributions
+  "a","ad","adf"...n substrings so contribution is just 9 
+
+=>there are 4 chars prev to e ..so all the substrings that start with these 4 chars and include 
+  'e' in them will make the contribution of e,there are 6 options for each char appearing before 
+  'e' and for 'e' itself (those 6 are to stop at e/b/f/z/v/i ) so total of (4+1)*(5+1)
+  (+1 becuz to include subtrings starting with 'e' itself).
+  
+  for 'a' the no. of substtrings where it include 'e' are :
+  "adfce","adfceb","adfcebz","adfcebzv","adfcebzvi" 
+  similarly substring starting with d,f,c and e(itself) ..has 6 subtrings including 'e'
+  
+=> for last 'i' all the chars (including itself)  will contribuite 1 time each 
+   (by including i in them in each of their substring(i will be last char in them))  
+  
+=> inshort the formula = 
+(no. of prev chars including itself) * (no. of chars later to that vowel ,including itself)
+    
+  
+  
+  */
