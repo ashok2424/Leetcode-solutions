fead78a4ab5b8bfd1d00081f89be3a3645b50af6
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-23 10:38:28 +0530

    Time: 4 ms (75.00%), Space: 10.9 MB (50.00%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& a) {
+        //sort in dec order and skip every 3rd elem
+        sort(a.begin(),a.end(),greater<int>());
+        
+        long long  ans  = 0; int c =1;
+        for(int i=0;i<a.size();i++){
+            if(c%3!=0) ans+=a[i];
+            c++;
+        }
+        
+        // for(int i=2;i<a.size();i+=3){
+        //     ans-=a[i];
+        // }
+        return ans;
+    }
+};
