4a1a31dd7505fd694c2edfd382882a42ebdb6932
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-23 10:20:14 +0530

    Time: 148 ms (88.62%), Space: 56 MB (17.22%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     long long maxRunTime(int n, vector<int>& batteries) {
         long long sum = 0 ; for(auto &e:batteries) sum+=e;
-        long long l = 0,h =sum;
-        long long ans =-1;
+        long long l = 0,h =sum,ans = -1;
         
         while(l<=h){
             long long mid = l + (h-l)/2;
             
-            long long run_time = n*mid, c = 0; 
+            long long tot_run_time = n*mid, c = 0; 
             for(auto &e:batteries){
                 c+=min((long long)e,mid);
             } 
             
-            if(c>=run_time){
-                l = mid+1; ans = mid;
+            if(c>=tot_run_time){
+                l = mid+1; ans = mid; 
             }
             else{ 
-                h = mid -1;
+                h = mid-1;
             }
         }
         return ans;
     }
 };
+//we want to run every computer mid minutes simultaneosly at samee time
+//so total time  = mid*no. of computers . so  if we can run we move l=mid+1
