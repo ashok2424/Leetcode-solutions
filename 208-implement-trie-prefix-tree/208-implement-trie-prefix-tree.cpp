6634baeeec8360007d3258a9b84e558b84cc6bb2
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-25 14:08:13 +0530

    Time: 94 ms (41.70%), Space: 45 MB (46.50%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,41 @@
+struct Node{
+    int ew ; //end words
+    int sw ; //start words (pref)
+    Node* next[26];
+};
+class Trie {
+public:
+    Node* root ;
+    Trie() {
+        root = new Node();
+    }
+    void insert(string word) {
+        Node* cur  = root ;
+        for(auto &ch:word){
+            if(cur->next[ch-'a']==NULL){
+                cur->next[ch-'a'] = new Node();
+            }
+            cur->sw++; 
+            cur = cur->next[ch-'a'];
+        }
+        cur->ew++;
+    }
+    bool search(string word) {
+        Node* cur = root;
+        for(auto &ch:word){
+            if(cur->next[ch-'a']==NULL) return false;
+            cur = cur->next[ch-'a'];
+        }
+        if(cur->ew>0) return true;
+        return false;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* cur = root;
+        for(auto &ch:prefix){
+            if(cur->next[ch-'a']==NULL) return false;
+            cur = cur->next[ch-'a'];
+        }
+        return true;
+    }
+};
