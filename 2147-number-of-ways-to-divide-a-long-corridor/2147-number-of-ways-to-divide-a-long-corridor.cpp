044d315bd7aa3aa667981497b33952642bb506d1
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-26 23:35:46 +0530

    Time: 219 ms (35.19%), Space: 30.5 MB (93.67%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,23 +1,23 @@
 class Solution {
-public:
+public://2147. Number of Ways to Divide a Long Corridor
     int numberOfWays(string s) {
         vector<int> v;long seats= 0,ans=1,plants=0;
+        
         for(auto &ch:s){
             if(ch=='S'){
                 seats++;
                 if(seats>2 and seats%2==1){
                     ans = (ans*(plants+1)) % (int)(1e9+7);
-                    // v.push_back(plants+1); 
-                    plants = 0;
+                    plants = 0; //set plants = 0 
                 }
             }
             if(ch=='P'){
                 if(seats>=2 and seats%2==0) plants++;
             }
         }
-        if(seats==0 or seats%2==1) return 0; //odd seats 
-        
-        // for(auto &e:v) ans = (ans*e)%(int)(1e9+7);
+    
+        if(seats==0 or seats%2==1) return 0; // odd seats/no seats
+    
         return ans;
     }
 };
@@ -27,4 +27,6 @@ public:
 // p  s_s  |p|p|p| s_s  |p|p|p|p|  s_s
 //         4 ways       5 ways 
       
+      
 // so by basic pnc 4*5  = 20 ways in total 
+//only count plants b/w two pairs of 2 chairs 
