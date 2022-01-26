35dffd829d0940fea88b520ccc02cfe89bd33f34
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-26 15:25:28 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (68.50%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -7,14 +7,15 @@ public:
             ch = tolower(ch);
             if(ch==t) c++;
         }
-        if((c==n ) or (c==n-1 and word[0]==toupper(word[0]))) return true;
-        int d=0;
-        for(auto ch:word){
-            char t = ch;
-            ch = toupper(ch);
-            if(ch==t) d++;
-        }
-        if(d==n) return true;
+        if(c==0 or c==n or (c==n-1 and word[0]==toupper(word[0]))) return true;
+        
+        // int d=0;
+        // for(auto ch:word){
+        //     char t = ch;
+        //     ch = toupper(ch);
+        //     if(ch==t) d++;
+        // }
+        // if(d==n) return true;
         return false;
     }
 };
