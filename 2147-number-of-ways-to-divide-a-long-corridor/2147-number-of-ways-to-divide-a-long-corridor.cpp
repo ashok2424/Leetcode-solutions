cefba3d55bc851c6467400b1c86b9509857e9def
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-26 23:29:13 +0530

    Time: 174 ms (60.23%), Space: 43.6 MB (44.75%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -6,6 +6,7 @@ public:
             if(ch=='S'){
                 seats++;
                 if(seats>2 and seats%2==1){
+                    ans = (ans*(plants+1)) % (int)(1e9+7);
                     v.push_back(plants+1); plants = 0;
                 }
             }
@@ -15,12 +16,17 @@ public:
         }
         if(seats==0 or seats%2==1) return 0; //odd seats 
         
-        for(auto &e:v){
-            // cout<<e<<" ";
-            ans = (ans*e)%(int)(1e9+7);
-        }
+        // for(auto &e:v){
+        //     // cout<<e<<" ";
+        //     ans = (ans*e)%(int)(1e9+7);
+        // }
         return ans;
     }
 };
+//   2seats        2seats          2seats
+// p  s_s  "p p p" s_s   "p p p p"  s_p_s
 
-// p s s "p p p" s s "p p p"  s s
+// p  s_s  |p|p|p| s_s  |p|p|p|p|  s_s
+//         4 ways       5 ways 
+      
+// so by basic pnc 4*5  = 20 ways in total 
