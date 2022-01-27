d0e96c996f94ff77aa4ff4808fe2f14d4d353bee
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-27 14:43:04 +0530

    Time: 216 ms (35.84%), Space: 83.3 MB (32.39%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -13,14 +13,35 @@ public:
         return true;
     }
 };
-/*
-The problem asks us to find whether the no. of glob_inv == to loc_inv ??
-And we know all loc_inv are glob. Why? .Cuz loc_inv are basically gobal with a dist as 1 b/w them.
+// The problem asks us to find whether the no. of glob_inv == to loc_inv ??
+// And we know all loc_inv are glob. Why? Cuz loc_inv are basically gobal with a dist as 1 b/w them.
     
-So if we can find at least one glob_inv which is not local our job is done & we return false
-And since we are maintaining the maximum value all the cases will be covered in it.
+// So if we can find at least one glob_inv which is not local our job is done & we return false
+// And since we are maintaining the maximum value all the cases will be covered in it.
 
-*/
+
+// for (int i = 0; i < A.length; i++) {
+//     if (Math.abs(i - A[i]) > 1)
+//         return false;
+// }
+
+// Elements to the right of A[i] must be bigger than A[i] except for A[i + 1].
+
+// Assuming N=4, i = 1 
+
+// A = [0,1, 2, 3] satisfy
+
+// // A[i] can switch with A[i + 1]
+// A = [0, 2, 1, 3] satisfy
+
+// // A[i] can switch with A[i - 1]
+// A = [1, 0, 2, 3] satisfy
+
+// but A = [0, 3, 2, 1] and A = [0, 3, 1, 2] wouldn't satisfy
+// So |i - A[i]| <= 1 is an invariant.
+
+
+
 
 // dont forget to type cast
 // for(int i = 0;i<(int)a.size()-2;i++) 
