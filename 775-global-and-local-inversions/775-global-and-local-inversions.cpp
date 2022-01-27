1171f31d0736f9064e33df058fe68adb3da09498
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-27 14:29:41 +0530

    Time: 188 ms (47.42%), Space: 83 MB (99.06%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -12,7 +12,7 @@ public:
 //         }
         
         int max_from_front = 0;int  n  = a.size();
-        for(int i = 0;i<n-2;i++){
+        for(int i = 0;i<(int)a.size()-2;i++){
             max_from_front = max(max_from_front,a[i]);
             if(max_from_front>a[i+2]) return false;
         }
