43f9d9a02d430944267d138caba4770d66b52615
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-27 14:36:52 +0530

    Time: 116 ms (96.56%), Space: 83.4 MB (32.39%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     bool isIdealPermutation(vector<int>& a) {
-//         int max_from_front = 0 ; int  n  = a.size();
+        int max_from_front = 0 ; int  n  = a.size();
         
-//         for(int i = 0;i<n-2;i++){
+        for(int i = 0;i<n-2;i++){
             
-//             max_from_front = max(max_from_front,a[i]);
-            
-//             if(max_from_front > a[i+2]) return false;
-            
-//         }
-        
-        int max_from_front = 0;int  n  = a.size();
-        for(int i = 0;i<(int)a.size()-2;i++){
             max_from_front = max(max_from_front,a[i]);
-            if(max_from_front>a[i+2]) return false;
+            
+            if(max_from_front > a[i+2]) return false;
+            
         }
-        return true;
-        
         return true;
     }
 };
+/*
+The problem asks us to find whether the no. of glob_inv == to loc_inv ??
+And we know all loc_inv are glob. Why? .Cuz loc_inv are basically gobal with a dist as 1 b/w them.
+    
+So if we can find at least one glob_inv which is not local our job is done & we return false
+And since we are maintaining the maximum value all the cases will be covered in it.
 
-// int cmax = 0, n = A.size();
-//         for (int i = 0; i < n - 2; ++i) {
-//             cmax = max(cmax, A[i]);
-//             if (cmax > A[i + 2]) return false;
-//         }
-//         return true;
+*/
+
+// dont forget to type cast
+// for(int i = 0;i<(int)a.size()-2;i++) 
 
 
 
