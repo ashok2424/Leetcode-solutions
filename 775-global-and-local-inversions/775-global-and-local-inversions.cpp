68f13949804b9a1c9ba4c2070bbcd4ad98414a9a
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-27 14:25:40 +0530

    Time: 108 ms (99.06%), Space: 83.3 MB (32.39%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    int merge(vector<int> & temp,vector<int> &a,int left,int mid,int right){
-        int inv = 0 , i = left , j = mid ,k = left;
-        while(i<=mid-1 and j<=right){
-            if(a[i]<=a[j]){
-                temp[k++] = a[i]; i++;
-            }else{
-                temp[k++] = a[j]; j++; inv += mid-i; 
-            }
+    bool isIdealPermutation(vector<int>& A) {
+        int cmax = 0, n = A.size();
+        for (int i = 0; i < n - 2; ++i) {
+            cmax = max(cmax, A[i]);
+            if (cmax > A[i + 2]) return false;
         }
-        while(i<=mid-1) {temp[k++] = a[i]; i++;}
-        while(j<=right) {temp[k++] = a[j]; j++;}
-        
-        for(int i = left;i<=right;i++) a[i] = temp[i];
-        return inv; //keep all sorted elems in temp back to a from left to right
-    }
-    
-    long count_inv(vector<int> &temp,vector<int> &a,int left,int right){
-        long inv = 0; 
-        if(left<right){
-            int mid = (left+right)/2;
-            
-            inv += count_inv(temp,a,left,mid);
-            inv += count_inv(temp,a,mid+1,right);
-            
-            inv += merge(temp,a,left,mid+1,right);
-        }
-        return inv;
-    }
-    
-    bool isIdealPermutation(vector<int>& a) {
-        int glob = 0,loc=0; vector<int>aa(a),temp(a.size());
-        //glob  = count inv in array using merge sort
-        glob = count_inv(temp,a,0,a.size()-1);//always take temp arr to merge
-        
-        for(int i=0;i<aa.size()-1;i++) if(aa[i]>aa[i+1]) loc++;  return glob==loc;
+        return true;
     }
 };
 
-          
+
+
+// int max_from_front = 0; int n  = a.size();
+        
+//         for(int i = 0;i<n-2;i++){
+            
+//             max_from_front = max(max_from_front,a[i]);
+            
+//             if(max_from_front > a[i+2]) return false;
+//         }
+//         return true;
