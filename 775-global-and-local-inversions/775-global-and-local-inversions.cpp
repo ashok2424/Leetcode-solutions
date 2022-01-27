a5ba5d49c04a1fc8d529c098394462164a4a66d3
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-27 14:08:15 +0530

    Time: 390 ms (5.32%), Space: 90.8 MB (5.16%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -13,28 +13,28 @@ public:
         while(j<=right) {temp[k++] = a[j]; j++;}
         
         for(int i = left;i<=right;i++) a[i] = temp[i];
-        return inv;
+        return inv; //keep all sorted elems in temp back to a from left to right
     }
-    long get_inv(vector<int> &temp,vector<int> &a,int left,int right){
+    
+    long count_inv(vector<int> &temp,vector<int> &a,int left,int right){
         long inv = 0;
         if(left<right){
             int mid = (left+right)/2;
             
-            inv += get_inv(temp,a,left,mid);
-            inv += get_inv(temp,a,mid+1,right);
+            inv += count_inv(temp,a,left,mid);
+            inv += count_inv(temp,a,mid+1,right);
             
             inv += merge(temp,a,left,mid+1,right);
         }
         return inv;
     }
+    
     bool isIdealPermutation(vector<int>& a) {
         int glob = 0,loc=0,mn = INT_MAX;vector<int>aa(a),temp(a.size());
         //glob  = count inv in array using merge sort
-        glob = get_inv(temp,a,0,a.size()-1);
+        glob = count_inv(temp,a,0,a.size()-1);//always take temp arr to merge
         
-        for(int i=0;i<aa.size()-1;i++){
-            if(aa[i]>aa[i+1]) loc++;
-        }
+        for(int i=0;i<aa.size()-1;i++) if(aa[i]>aa[i+1]) loc++;
         return glob==loc;
     }
 };
