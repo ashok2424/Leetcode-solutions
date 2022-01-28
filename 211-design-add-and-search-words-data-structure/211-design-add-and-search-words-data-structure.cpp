439c0a92d517d05864b568a56d8c3088d32bc500
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-28 14:42:43 +0530

    Time: 92 ms (69.53%), Space: 43.3 MB (74.45%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,46 @@
+class Node{
+    public:
+    int ew ; int sw ; //end words& start words (pref)
+    Node* next[26];
+};
+
+class WordDictionary {
+public:
+    Node* root =new Node();
+    WordDictionary() {
+        
+    }
+    
+    void addWord(string word) {
+        Node* cur  = root ;
+        for(auto &ch:word){
+            if(cur->next[ch-'a']==NULL){
+                cur->next[ch-'a'] = new Node();
+            }
+            cur->sw++; 
+            cur = cur->next[ch-'a'];
+        }
+        cur->ew++;
+    }
+    bool search(string word) {
+        Node* cur = root;
+        for(auto &ch:word){
+            if(ch=='.'){
+                for(char c='a';c<='z';c++){
+                    ch=c; if(search(word)) return true;
+                }
+            }
+            if(cur->next[ch-'a']==NULL) return false;
+            cur = cur->next[ch-'a'];
+        }
+        if(cur->ew>0) return true;
+        return false;
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
