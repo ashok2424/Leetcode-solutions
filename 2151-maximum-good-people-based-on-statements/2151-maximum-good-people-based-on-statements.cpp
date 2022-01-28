bc934f691819926f0adc1114b6ea9bc67d707836
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-28 11:46:34 +0530

    Time: 534 ms (27.37%), Space: 8.6 MB (24.20%) - LeetHub

--- 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
    int ans  = 0; vector<vector<int>> a;
-    void sub(int n, int i, string &temp,int good){
+    void subsets(int n, int i, string &temp,int good){
         if(i==n){
             if(is_valid(temp,a)) ans = max(ans,good);
             return;
         }
         temp.push_back('1');
-        sub(n, i+1, temp,good+1);
+        subsets(n, i+1, temp,good+1);
         temp.pop_back();
         
         temp.push_back('0');  
-        sub(n, i+1, temp,good);
+        subsets(n, i+1, temp,good);
         temp.pop_back();
     }
     
     int maximumGood(vector<vector<int>>& nums) {
-        string temp = ""; a = nums;
-        sub(a.size(), 0,temp,0); 
-        return ans;
+        string temp = ""; a = nums;//using temp as vector<int> gives TLE 
+        subsets(a.size(), 0,temp,0);  return ans;
     }
+    
     bool is_valid(string &temp,vector<vector<int>>& a){
+        
         for(int i = 0;i<temp.size();i++){
             if(temp[i]=='0') continue;//we care only about truth speaker
+            
             for(int j = 0;j<temp.size();j++){
+                //if contradictions return false;
                 if(a[i][j]==1 and temp[j]-'0'==0) return false;
                 if(a[i][j]==0 and temp[j]-'0'==1) return false;
             }
