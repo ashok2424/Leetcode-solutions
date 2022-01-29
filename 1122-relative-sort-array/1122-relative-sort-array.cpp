62d44dc7becd16b3aabedfb497ee3e9e4af87f9d
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-29 11:32:00 +0530

    Time: 3 ms (78.97%), Space: 7.9 MB (38.14%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& a, vector<int>& b) {
-        unordered_map<int,queue<int>>mp;
-        for(int i=0;i<a.size();i++) mp[a[i]].push(i);
-        vector<int> ans;
+        unordered_map<int,int>mp; vector<int> ans;
+        
+        for(int i=0;i<a.size();i++) mp[a[i]]++;
+        
         for(auto &e:b){
-            while(mp[e].size()!=0){
-                ans.push_back(a[mp[e].front()]); mp[e].pop();
+            while(mp[e]!=0){
+                ans.push_back(e); mp[e]--;
             }
         }
+        
+        //for those elems not in b arr
         sort(a.begin(),a.end());
         for(auto &e:a){
-            if(mp[e].size()!=0) ans.push_back(e);
+            if(mp[e]!=0) ans.push_back(e);
         }
         return ans;
     }
 };
+
+//for those elems not in b arr u can take them into 
+//seperate arr and sort them instead sorting whole "a" arr
