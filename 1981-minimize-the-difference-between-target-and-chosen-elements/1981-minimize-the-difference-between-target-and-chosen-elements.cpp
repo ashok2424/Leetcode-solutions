3c0cf40f8bf41eaf6d8c920edc4bd0bcdc84c424
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-01-30 14:51:00 +0530

    Time: 1256 ms (48.23%), Space: 14.5 MB (59.76%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,8 +1,7 @@
 class Solution {
-public: int dp[71][56000];
-    vector<vector<int>> a; int diff = INT_MAX ;
+public: int dp[71][5041] ;
     
-    int solve(int i,int sum,int k){
+    int solve(int i,int sum,int &k,vector<vector<int>> &a){
         if(i==a.size()){
             return  abs(sum-k);
         }
@@ -10,13 +9,14 @@ public: int dp[71][56000];
         
         int ans = INT_MAX;
         for(int j = 0;j<a[i].size();j++){
-            ans = min(ans,solve(i+1,sum + a[i][j],k));
+            ans = min(ans,solve(i+1,sum + a[i][j],k,a));
+            if(ans==0) break;
         }
+        
         return dp[i][sum] =  ans;
     }
     
     int minimizeTheDifference(vector<vector<int>>& mat, int k) {
-        memset(dp,-1,sizeof(dp));
-        a = mat; return solve(0,0,k);
+        memset(dp,-1,sizeof(dp)); return solve(0,0,k,mat);
     }
 };
