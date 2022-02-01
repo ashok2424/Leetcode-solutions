7bd0556aa742d97bc7cae6b51c44f884d4674c3c
Ashok Molagavalli <97282425+ashok2424@users.noreply.github.com>
2022-02-01 16:12:51 +0530

    Time: 11 ms (83.54%), Space: 8.4 MB (35.19%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        
-        string str = "abc"; int size = str.size();
-
-        while(s.find(str)!=string ::npos){
-            
-            int ind = s.find(str); 
-            s.erase(ind,size);
+    bool isValid(string a) {
+        stack<char>st; 
+        for(auto &ch:a){
+            if(ch=='c'){
+                if(st.size()<2){
+                    return false;
+                }else{
+                    char s=st.top(); st.pop();
+                    char f=st.top(); st.pop();
+                    
+                    if(f!='a' or s!='b') return false;
+                }
+            }
+            else{
+                st.push(ch); //dont push 'c'
+            }
         }
-        return s=="";
+        return st.size()==0;
     }
 };
+// string str = "abc"; int size = str.size();
+
+// while(s.find(str)!=string ::npos){
+
+//     int ind = s.find(str); 
+//     s.erase(ind,size);
+// }
+// return s=="";
